6afb24ff6dfb624d6d7ce78c051a9cfafe1974e3
eniallator <eniallatormc@gmail.com>
2019-02-07 08:52:30 +0000

    Added more to the Game class

--- src/Game.py
+++ src/Game.py
@@ -1,7 +1,22 @@
+from Base_Cell import Base_Cell
+
+
 class Game:
 
+    def __init__(self, width, height):
+        self.__grid = []
+        for j in range(height):
+            row = [None for i in range(width)]
+            self.__grid.append(row)
+
     def update(self):
-        pass
+        for row in self.__grid:
+            for cell in row:
+                if cell is not None and Base_Cell in cell.__bases__:
+                    cell.update()
 
     def draw(self):
-        pass
+        for row in self.__grid:
+            for cell in row:
+                if cell is not None and Base_Cell in cell.__bases__:
+                    cell.draw()
