0100275b093d2c5d38a4775bab09b18ac43c6998
eniallator <eniallatormc@gmail.com>
2019-02-08 12:27:40 +0000

    Added test code for testing it

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from src.Game import Game
 
-GAME = Game(50, 50)
+GAME = Game(15, 15)
+
+from src.Cells.Gol_Cell import Gol_Cell
+GAME.set_sequence([
+    [None, Gol_Cell(), None],
+    [None, Gol_Cell(), Gol_Cell()],
+    [Gol_Cell(), Gol_Cell(), None]
+])
+
+GAME.draw()
 
 
 def main():
-    GAME.update()
-    GAME.draw()
+    for i in range(3):
+        GAME.update()
+        GAME.draw()
 
 
 if __name__ == '__main__':
